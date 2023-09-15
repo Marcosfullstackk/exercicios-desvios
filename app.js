ac4729f5ca94a4434ab0a53a83e89843a6414d66
Marcos Vinicius da Silva <PET0154085@aluno.firjansenaisesi.com.br>
2023-09-14 21:48:04 -0300

    lacos de repetição

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
+function lacos_de_repeticao_exercicio1(){
+    
+    let verificador = false 
+
+    while ( verificador == false ) {
+        let numero1 = prompt ("escreva quantos segundos vc deseja.")
+
+        if (numero1 <= 59) {
+            for( let i = 1 ; i <= numero1; i++ ){
+                alert(i)
+                console.log(i)
+            } 
+            verificador = true
+            
+        } else {
+            alert("Número errado")
+        } 
+    }
+}
+
+function lacos_de_repeticao_exercicio2(){
+
+
+        let mumerodigitado = prompt ("digita um numero")
+
+        let numeroditado = prompt ("digit um numero")
+
+
+for(let i=1 ; i<=9 ; i++ ){
+    console.log(`${mumerodigitado} X ${ i } = ${( mumerodigitado * i) }`)
+
+}
+    }
+
+
+function lacos_de_repeticao_exercicio3(){
+
+}let numeroZ = prompt("digite um numero")
+
+
+
+lacos_de_repeticao_exercicio3()
+
+
 
